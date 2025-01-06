b480c852cb89a0cadd4f1d78bfb086caf68f4773
FIRST_NAME LAST_NAME <mail@adress.com>
2025-01-07 02:08:17 +0300

    Select statement içeriği yüklendi.

---
+++ Select Statement in MySQL.sql
@@ -0,0 +1,13 @@
+SELECT * FROM parks_and_recreation.employee_demographics;
+
+SELECT * FROM parks_and_recreation.employee_salary;
+
+SELECT * FROM parks_and_recreation.parks_departments; 
+
+Select first_name, age from parks_and_recreation.employee_demographics;
+
+Select first_name, age, age+10 , (age+10) * 10 + 20 -10 from parks_and_recreation.employee_demographics;
+
+Select DISTINCT gender from parks_and_recreation.employee_demographics;
+
+Select DISTINCT first_name,gender from parks_and_recreation.employee_demographics;
